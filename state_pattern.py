aa92ccf82e70eafd574c23fd053d6d7524fd0c00
ARK <arkfreestyle95@gmail.com>
2020-10-31 10:36:26 +0500

    Completed State Pattern.

--- state_pattern.py
+++ state_pattern.py
@@ -18,7 +18,7 @@ We'll build a simple XML parser to illustrate this pattern.
 """
 
 xml_to_parse = \
-"""
+    """
 <book>
     <author>Dusty Phillips</author>
     <publisher>Packt Publishing</publisher>
@@ -57,12 +57,12 @@ class Parser:
         self.root = None
         self.current_node = None
         self.state = FirstTag()
-    
+
     def process(self, remaining_string):
         remaining = self.state.process(remaining_string, self)
         if remaining:
             self.process(remaining)
-    
+
     def start(self):
         self.process(self.parse_string)
 
@@ -71,36 +71,72 @@ class FirstTag:
     def process(self, remaining_string, parser):
         i_start_tag = remaining_string.find('<')
         i_end_tag = remaining_string.find('>')
-        tag_name = remaining_string[i_start_tag+1:i_end_tag]
+        tag_name = remaining_string[i_start_tag + 1:i_end_tag]
         root = Node(tag_name)
         parser.root = parser.current_node = root
         parser.state = ChildNode()
-        return remaining_string[i_end_tag+1:]
+        return remaining_string[i_end_tag + 1:]
 
 
 class ChildNode:
     def process(self, remaining_string, parser):
         stripped = remaining_string.strip()
-        
+
         if stripped.startswith("</"):
             parser.state = CloseTag()
-        
+
         elif stripped.startswith("<"):
             parser.state = OpenTag()
-        
+
         else:
             parser.state = TextNode()
-        
+
         return stripped
 
 
 class OpenTag:
-    pass
+    def process(self, remaining_string, parser):
+        i_start_tag = remaining_string.find("<")
+        i_end_tag = remaining_string.find(">")
+        tag_name = remaining_string[i_start_tag + 1:i_end_tag]
+        node = Node(tag_name, parser.current_node)
+        parser.current_node.children.append(node)
+        parser.current_node = node
+        parser.state = ChildNode()
+        return remaining_string[i_end_tag + 1:]
 
 
 class CloseTag:
-    pass
+    def process(self, remaining_string, parser):
+        i_start_tag = remaining_string.find('<')
+        i_end_tag = remaining_string.find('>')
+        assert remaining_string[i_start_tag + 1] == "/"
+        tag_name = remaining_string[i_start_tag + 2:i_end_tag]
+        assert tag_name == parser.current_node.tag_name
+        parser.current_node = parser.current_node.parent
+        parser.state = ChildNode()
+        return remaining_string[i_end_tag + 1:].strip()
 
 
 class TextNode:
-    pass
+    def process(self, remaining_string, parser):
+        i_start_tag = remaining_string.find('<')
+        text = remaining_string[:i_start_tag]
+        parser.current_node.text = text
+        parser.state = ChildNode()
+        return remaining_string[i_start_tag:]
+
+
+if __name__ == "__main__":
+    import sys
+    
+    with open(sys.argv[1]) as file:
+        contents = file.read()
+        p = Parser(contents)
+        p.start()
+        nodes = [p.root]
+
+        while nodes:
+            node = nodes.pop(0)
+            print(node)
+            nodes = node.children + nodes
